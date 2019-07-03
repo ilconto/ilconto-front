bc8b8cae39b1f6bda37999706ec38ce4b76119ad
paul <pazoulai@gmail.com>
2019-07-04 00:45:53 +0200

    added template for creating board

---
+++ src/components/CreateBoard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    # implement board creation form here
+    <router-link to="login">Login to existing board</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateBoard",
+  data() {
+    return {
+      boardName: null
+    };
+  }
+};
+</script>
+
+
+<style lang="sass" scoped>
+.boardHeader 
+    margin-bottom: 3em; 
+
+    
+</style>
