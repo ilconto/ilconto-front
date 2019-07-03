40a0bc2490203cc1a7d17bdf9b04fa423548e5a9
paul <pazoulai@gmail.com>
2019-07-04 00:46:17 +0200

    added component for login

---
+++ src/components/LoginBoard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    # implement login here
+    <router-link to="create">Create a new board</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginBoard",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
+
+
