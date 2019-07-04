43dc494797e9ac7d80d83daf98c22244d8c4701c
paul <pazoulai@gmail.com>
2019-07-05 01:08:21 +0200

    Changed order of buttons

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
+    <h1 class="title is-1">Welcome to Ilconto</h1>
+
     <p>
-      <router-link :to="{name: 'create'}">Create a new board</router-link>
+      <router-link :to="{name: 'login'}">
+        <b-button type="is-primary">Login a board</b-button>
+      </router-link>
     </p>
-    <p>Or</p>
+    <p id="or">Or</p>
     <p>
-      <router-link :to="{name: 'login'}">Login to an existing one</router-link>
+      <router-link :to="{name: 'create'}">
+        <b-button type="is-primary">Create a new board</b-button>
+      </router-link>
     </p>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+
+<style lang="scss">
+</style>
