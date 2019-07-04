6a8f4810928d201e6fafe385fd384f036e9c78d3
paul <pazoulai@gmail.com>
2019-07-05 00:15:46 +0200

    Added landing page

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>
+      <router-link :to="{name: 'create'}">Create a new board</router-link>
+    </p>
+    <p>Or</p>
+    <p>
+      <router-link :to="{name: 'login'}">Login to an existing one</router-link>
+    </p>
+  </div>
+</template>
