004f4ddd9ec81cee857f98ff993ff22bede51403
paul <pazoulai@gmail.com>
2019-07-05 01:07:34 +0200

    Added first fields for creating a board

--- src/components/CreateBoard.vue
+++ src/components/CreateBoard.vue
@@ -1,7 +1,31 @@
 <template>
   <div>
-    <router-link :to="{name:'root'}">Back</router-link>
-    <p># implement board creation form here</p>
+    <h1 class="title is-1">Create a new board here</h1>
+
+    <div class="inputs">
+      <b-field label="Board Name">
+        <b-input v-model="boadId" placeholder="you board name"></b-input>
+      </b-field>
+      <b-field label="Board Password">
+        <b-input v-model="boardPassword" placeholder="superSafePass" type="password"></b-input>
+      </b-field>
+
+      <b-field label="Members">
+        <b-input v-model="emailToAdd" placeholder="example@domain.com" type="email"></b-input>
+      </b-field>
+      <p>
+        <b-button type="is-light">Add member</b-button>
+      </p>
+    </div>
+
+    <p>
+      <b-button type="is-primary">Create Board</b-button>
+    </p>
+    <p id="back">
+      <router-link :to="{name:'root'}">
+        <b-button type="is-info">Back</b-button>
+      </router-link>
+    </p>
   </div>
 </template>
 
