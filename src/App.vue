043c0fb1e705de5e1528b5e8bc634b7e5c0898cd
paul <pazoulai@gmail.com>
2019-07-05 01:05:33 +0200

    Moved global style into #app selector

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,20 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap");
 #app {
-  /* font-family: "Permanent Marker", cursive; */
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
   padding: 1em;
+  button {
+    margin: 1em;
+  }
+  .title {
+    font-family: "Permanent Marker", cursive;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 }
 </style>
