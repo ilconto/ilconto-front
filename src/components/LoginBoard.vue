e6cbe03265fb64f60240a3f0364bd34338aba139
paul <pazoulai@gmail.com>
2019-07-06 00:35:00 +0200

    reimplemented most of the form

--- src/components/LoginBoard.vue
+++ src/components/LoginBoard.vue
@@ -1,21 +1,23 @@
 <template>
-  <div>
-    <h1 class="title is-1">Login to an existing board here</h1>
+  <div class="container">
+    <div class="centered-constrained">
+      <h1 class="title is-1">Login to an existing board here</h1>
 
-    <b-field label="Board ID">
-      <b-input v-model="boadId" placeholder="you board unique id"></b-input>
-    </b-field>
-    <b-field label="Board Password">
-      <b-input v-model="boardPassword" placeholder="superSafePass" type="password"></b-input>
-    </b-field>
-    <p>
-      <b-button type="is-primary">Login</b-button>
-    </p>
-    <p id="back">
-      <router-link :to="{name:'root'}">
-        <b-button type="is-info">Back</b-button>
-      </router-link>
-    </p>
+      <b-field label="Board ID">
+        <b-input v-model="boadId" placeholder="you board unique id"></b-input>
+      </b-field>
+      <b-field label="Board Password">
+        <b-input v-model="boardPassword" placeholder="superSafePass" type="password"></b-input>
+      </b-field>
+      <p>
+        <b-button type="is-primary">Login</b-button>
+      </p>
+      <p id="back">
+        <router-link :to="{name:'root'}">
+          <b-button type="is-info">Back</b-button>
+        </router-link>
+      </p>
+    </div>
   </div>
 </template>
 
