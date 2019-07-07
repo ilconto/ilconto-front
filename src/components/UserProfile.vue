95a7498daadc99642cb934fc1a73bba1bdfb63c4
paul <pazoulai@gmail.com>
2019-07-07 11:57:09 +0200

    Added user profile page

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1 class="title is-1">Welcome {{this.$session.get("user").name}}</h1>
+    <h2 class="title is-3">Your boards:</h2>
+    <ul>
+      <li v-for="board in boards" v-bind:key="board.id">
+        <div class="level">
+          <span class="level-item">{{ board.name }}</span>
+          <span class="level-item">
+            <router-link :to="{name: 'board', params: {id: 5}}">
+              <b-button type="is-primary">View</b-button>
+            </router-link>
+          </span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import { userBoards } from "../mock.js";
+
+export default {
+  data() {
+    return {
+      boards: null
+    };
+  },
+  created() {
+    this.boards = this.$session.get("user").boards;
+  },
+  methods: {
+    // getUserBoards: getUserBoards
+  }
+};
+</script>
