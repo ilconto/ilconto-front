93c26a1dc332acd901a160a640a57c30b5faa99a
paul <pazoulai@gmail.com>
2019-07-07 11:56:25 +0200

    Added mock data

---
+++ src/mock.js
@@ -0,0 +1,42 @@
+export const board = {
+    id: "3",
+    title: "MyBoard",
+    members: [{
+            id: "34",
+            name: "Paul",
+            email: "pe@hovosotig.ae",
+            last_time: "15624148",
+            board: "3"
+        },
+        {
+            id: "50",
+            name: "Hugo",
+            email: "idedi@puufa.mh",
+            last_time: "15624148",
+            board: "3"
+        },
+        {
+            id: "84",
+            name: "Maxence",
+            email: "icrab@toc.pk",
+            last_time: "15624148",
+            board: "3"
+        }
+
+    ]
+}
+
+
+export const userBoards = [{
+        id: "3",
+        name: "board1"
+    },
+    {
+        id: "4",
+        name: "board8"
+    },
+    {
+        id: "5",
+        name: "board9"
+    },
+]
