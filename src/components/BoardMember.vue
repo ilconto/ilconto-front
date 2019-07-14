b582ec4afeb09c6f8b2c57eaebbc735e20a22ceb
paul <pazoulai@gmail.com>
2019-07-14 23:39:23 +0200

    Adds reset functionality

--- src/components/BoardMember.vue
+++ src/components/BoardMember.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="main level has-background-primary">
-    <span class="level-item">{{member.name}}</span>
-    <span class="level-item score">{{computeScore(member.last_time)}} days</span>
-  </div>
+  <span>
+    <div class="main level has-background-primary">
+      <span class="level-item">{{member.name}}</span>
+      <span v-if="isOwner">
+        <b-button @click="confirmReset" type="is-danger">reset my score</b-button>
+      </span>
+      <span class="level-item score">{{score}} days</span>
+    </div>
+  </span>
 </template>
 
 <script>
+import axios from "axios";
+
 function computeScore(utcEpochTime) {
   var now = Math.floor(Date.now() / 1000); //seconds
   var deltaSeconds = now - utcEpochTime;
@@ -13,15 +20,54 @@ function computeScore(utcEpochTime) {
 }
 
 function secondsToHumanReadable(seconds) {
-  var days = Math.floor(seconds / (3600 * 24)); //days
+  var days = Math.floor(seconds / (3600 * 24)) + 1; //days
   return days;
 }
 
 export default {
   name: "BoardMember",
-  props: ["member"],
+  props: ["member", "boardID"],
+  data() {
+    return {
+      username: "",
+      isOwner: false,
+      boards: [],
+      score: 0
+    };
+  },
   methods: {
-    computeScore: computeScore
+    computeScore: computeScore,
+    created() {},
+    confirmReset() {
+      var boardID = this.boardID;
+      this.$dialog.confirm({
+        title: "Resetting your score",
+        message:
+          "Are you sure you want to <b>reset</b> your score? This action cannot be undone.",
+        confirmText: "Reset score",
+        type: "is-danger",
+        hasIcon: true,
+        onConfirm: () => {
+          this.score = computeScore(Date.now() / 1000);
+          axios({
+            method: "patch",
+            baseURL: process.env.VUE_APP_ROOT_API,
+            url: `/boards/${boardID}/reset/${this.$session.get("user").id}`
+          })
+            .then(response => {})
+            .catch(error => {
+              this.score = computeScore(this.member.last_time); // abort modification if request failed
+              console.log(error);
+            });
+        }
+      });
+    }
+  },
+  mounted() {
+    if (this.member.name == this.$session.get("user").username) {
+      this.isOwner = true;
+    }
+    this.score = computeScore(this.member.last_time);
   }
 };
 </script>
