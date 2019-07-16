696422da603b2bdfb2e77927a9295a75e8365392
paul <pazoulai@gmail.com>
2019-07-16 23:17:09 +0200

    adds background image

--- src/App.vue
+++ src/App.vue
@@ -19,17 +19,33 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   padding: 1em;
-  button {
+  border-style: solid;
+  border-width: 0.5em;
+  border-radius: 0.5em;
+  // background-color: rgba(91, 158, 155, 0.534);
+  background-image: url("~@/assets/dark-paths.png");
+  background-size: 30vh;
+  height: 100vh;
+  margin-top: 0;
+  b button {
     margin: 1em;
   }
-  .title {
+  .marker {
     font-family: "Permanent Marker", cursive;
+    color: white;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }
+  .title {
+    @extend .marker;
   }
   .centered-constrained {
     max-width: 15cm;
     margin: 0 auto;
+    margin-top: 15%;
+  }
+  button {
+    width: 100%;
   }
 }
 </style>
