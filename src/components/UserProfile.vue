59d0003abbc89171af98bf2b462302263b698bee
paul <pazoulai@gmail.com>
2019-09-23 15:35:55 +0200

    adds real values from api for username and boards list

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="container" id="profile-page">
     <Header :subtitle="false" />
+    <div class="is-title">Welcome, {{this.$session.get("username")}}</div>
     <div id="board-list">
+      <BoardListItem
+        v-for="(item, id) in this.$session.get('boards')"
+        v-bind:title="item.board.title"
+        v-bind:key="id"
+      />
       <BoardListItem />
       <BoardListItem />
       <BoardListItem />
@@ -25,7 +31,40 @@ export default {
       boards: []
     };
   },
-  created() {}
+  beforeCreate() {
+    axios({
+      method: "get",
+      baseURL: process.env.VUE_APP_ROOT_API,
+      url: "/profile/",
+      json: true,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${this.$session.get("token")}`
+      }
+    })
+      .then(response => {
+        this.$session.set("username", response.data.username);
+        this.$session.set("email", response.data.email);
+
+        this.$session.set("boards", response.data.memberships);
+      })
+      .catch(e => {
+        this.$snackbar.open({
+          message: "Could not log you in. Please try again",
+          type: "is-warning",
+          position: "is-bottom",
+          actionText: "create an account",
+          queue: false,
+          duration: 5000,
+          onAction: () => {
+            this.$router.push("/signin");
+          }
+        });
+
+        this.email = "";
+        this.password = "";
+      });
+  }
 };
 </script>
 
