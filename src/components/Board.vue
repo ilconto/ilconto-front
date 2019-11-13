4485aeb5018432a34592d70f3986c24827e35d69
paul <pazoulai@gmail.com>
2019-11-13 22:14:20 +0100

    Started working on board details page

---
+++ src/components/Board.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container item">
+    <p>
+      <span>{{title}}</span>
+    </p>
+    
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+
+
+export default {
+
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    get_board_details() {
+      axios({
+        method: "get",
+        baseURL: process.env.VUE_APP_ROOT_API,
+        url: `/boards/${this.$route.params.boardId}`,
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Token ${this.$session.get("token")}`
+        }
+      })
+      .then(response => {
+        this.title = response.data.title;
+      })
+      .catch(e => {
+        console.log(e)
+      })
+    }
+  },
+  beforeMount() {
+    this.get_board_details();
+  }
+};
+</script>
