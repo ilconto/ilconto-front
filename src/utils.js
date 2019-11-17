f9417c27a7d559a3335db784caf8274dfda00866
paul <pazoulai@gmail.com>
2019-11-17 22:25:43 +0100

    adds function to display score as string

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,30 @@ const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[
 export function isEmailValid(email) {
     return (email == "") ? "" : (reg.test(email)) ? true : false;
 }
+
+export function secondsToDays(utcSeconds) {
+    var days = Math.floor(utcSeconds / (3600 * 24))
+    var hours = Math.floor(utcSeconds / 3600 - (days * 24))
+    var minutes = Math.floor(utcSeconds / 60 - days * 24 * 60 - hours * 60)
+
+    var output = ""
+    if (days) {
+        output += days + " d, "
+    }
+    if (hours) {
+        output += hours + " h, "
+    }
+    if (minutes) {
+        output += minutes + " min, "
+    }
+    return output.slice(0, -2) // removing the last ", "
+}
+
+export function utcTimeToRelativeSeconds(utcSeconds) {
+    var now = (new Date().getTime()) / 1000;
+    return now - utcSeconds
+}
+
+export function utcTimeToRelativeDays(utcSeconds) {
+    return secondsToDays(utcTimeToRelativeSeconds(utcSeconds))
+}
