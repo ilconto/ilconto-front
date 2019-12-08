8576f6ecb7bfcab089a5673d931347a3c33c3b8b
paul <pazoulai@gmail.com>
2019-12-08 16:46:02 +0100

    Improves board member item

--- src/components/BoardMemberItem.vue
+++ src/components/BoardMemberItem.vue
@@ -1,15 +1,27 @@
 <template>
-<v-container>
+  <v-container>
     <v-layout row class="member">
-    <v-flex md6 class="member-username">
-      {{user.username}}
-    </v-flex>
-    <v-flex md6 class="member-delay" color="secondary">
-      {{formatTime(user.score)}}
-    </v-flex>
-    <!-- <v-if> -->
-      <v-btn id="reset-btn" @click=";" color="warning">Reset</v-btn>
-    <!-- </v-if> -->
+      <v-flex md6 class="member-username">{{user.username}}</v-flex>
+      <v-flex md6 class="member-delay" color="secondary">{{formatTime(user.score)}}</v-flex>
+      <v-dialog v-if="isAdmin" v-model="resetConfirmation">
+        <template v-slot:activator="{ on }">
+          <v-btn id="reset-btn" color="warning" v-on="on">Reset</v-btn>
+        </template>
+        <template>
+          <div class="modal-content">
+            <p>Do you really want to reset your score to now?</p>
+            <div id="btns">
+              <v-btn @click="cancelReset" class="modal-btn" color="primary">Cancel</v-btn>
+              <v-btn @click="confirmReset" class="modal-btn" color="secondary">Confirm</v-btn>
+            </div>
+          </div>
+        </template>
+      </v-dialog>
+    </v-layout>
+  </v-container>
+</template>
+    </v-dialog>
+    
     
   </v-layout>
   
@@ -31,66 +43,105 @@
   margin: auto;
 }
 .member-delay {
-  color: var(--v-secondary-base)
+  color: var(--v-secondary-base);
 }
 #reset-btn {
   width: 2em;
+}
+#btns {
+  float: right;
+}
+.modal-content {
+  width: 100%;
+  padding: 1em;
+  /* padding-top: 2em; */
+  /* background-color: rgba(255, 255, 255, 0.2); */
+}
+
+.modal-btn {
+  margin: 0.5em;
 }
 </style>
 
 
 
 <script>
-
-import moment from 'moment'
-import {utcTimeToRelativeDays, utcTimeToRelativeSeconds, secondsToDays} from "../utils"
+import {
+  utcTimeToRelativeDays,
+  utcTimeToRelativeSeconds,
+  secondsToDays
+} from "../utils";
+import AskResetConfirmation from "./modals/AskResetConfirmation";
+import axios from "axios";
 
 export default {
   name: "BoardMember",
+  components: {
+    AskResetConfirmation
+  },
   props: {
     user: {
       type: Object,
       required: true
+    },
+    parentBoardId: {
+      type: String,
+      required: true
     }
+  },
+  data() {
+    return {
+      isAdmin: false,
+      resetConfirmation: false
+      // parentBoardId: "blz"
+    };
   },
 
   computed: {
     readableDelay: function() {
-      return moment.duration(this.delay).humanize()
+      return moment.duration(this.delay).humanize();
     }
   },
 
   methods: {
-      formatTime(seconds) {
-      return secondsToDays(utcTimeToRelativeSeconds(seconds))
+    formatTime(seconds) {
+      return secondsToDays(utcTimeToRelativeSeconds(seconds));
     },
-  //   confirmReset() {
-  //     var boardID = this.boardID;
-  //     this.$dialog.confirm({
-  //       title: "Resetting your score",
-  //       message:
-  //         "Are you sure you want to <b>reset</b> your score? This action cannot be undone.",
-  //       confirmText: "Reset score",
-  //       type: "is-danger",
-  //       hasIcon: true,
-  //       onConfirm: () => {
-  //         this.score = computeScore(Date.now() / 1000);
-  //         axios({
-  //           method: "patch",
-  //           baseURL: process.env.VUE_APP_ROOT_API,
-  //           url: `/boards/${boardID}/reset/${this.$session.get("user").id}`
-  //         })
-  //           .then(response => {})
-  //           .catch(error => {
-  //             this.score = computeScore(this.member.last_time); // abort modification if request failed
-  //             console.log(error);
-  //           });
-  //       }
-  //     });
-  //   }
-  // },
-  // mounted() {
-    
+    askResetConfirmation() {
+      this.resetConfirmation = true;
+    },
+    cancelReset() {
+      this.resetConfirmation = false;
+    },
+    confirmReset() {
+      console.log(this.parentBoardId);
+      console.log(this.$session.get("userID"));
+      var bodyFormData = new FormData();
+      bodyFormData.set("score", new Date().getTime());
+      axios({
+        method: "patch",
+        baseURL: process.env.VUE_APP_ROOT_API,
+        url: `/boards/${this.parentBoardId}/members/${this.$session.get(
+          "userID"
+        )}`,
+        data: bodyFormData,
+        json: true,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${this.$session.get("token")}`
+        }
+      })
+        .then(response => {})
+        .catch(error => {
+          this.score = computeScore(this.member.last_time); // abort modification if request failed
+          console.log(error);
+        });
+    }
+  },
+  mounted() {
+    console.log(this.$session.get("username"));
+    this.isAdmin = this.$session.get("username") == this.user.username;
+    console.log(this.parentBoardId);
   }
 };
 </script>
