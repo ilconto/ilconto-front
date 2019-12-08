ebe7e9b25ab31cd3f9a2ff73e799cbff7d292bd8
paul <pazoulai@gmail.com>
2019-12-08 16:48:59 +0100

    adds modal for reset comfirmation

---
+++ src/components/modals/AskResetConfirmation.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>Do you really want to reset your score to now?</p>
+        <v-spacer></v-spacer>
+        <div id="btns">
+            <v-btn color="primary">Cancel</v-btn>
+            <v-btn color="secondary">Confirm</v-btn>
+        </div>
+        
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+#btns {
+    float: right;
+
+}
+
+button {
+    margin: 0.5em
+}
+</style>
